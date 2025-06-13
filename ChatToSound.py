1f9e2cbb1e4869e2315ab09cf6b633b7d9350772
anananabpcM <122233939+Yxor257@users.noreply.github.com>
2025-06-12 22:34:11 -0500

    ChatToSound.py

--- ChatToSound.py
+++ ChatToSound.py
@@ -6,9 +6,16 @@ import datetime
 streamID = ''
 chat = pytchat.create(streamID)
 soundQ = []
+#DO YOU HAVE A MAIL SOUND IF SO SET THIS TO TRUE AND SCROLL TO THE BOTTOM TO CHANGE THE INPUT KEY
+hasRecommendationSound = False
+#single words only
 hotKeyword = []
+# place multi long words as a sentence (DOES NOT FUNCTION)
+#multi_Long_Keywords = ['example statement', 'example split']
+# you can put all the keyword stuff in here
 hotKeyFind = {}
 punctuation = '@#$%^&*()_+=-`;:"?/>.<,`~|'
+#DO NOT EDIT RECOMMEND OR MUTED PEOPLE LIST VAR'S
 recommend = []
 mutedPeople = []
 
@@ -25,29 +32,33 @@ while chat.is_alive():
         with open("MUTED_PEOPLE_FOR_SOUND_EFFECTS.txt", "r") as file:
             unsplitData = file.read()
             mutedPeople = unsplitData.split()
-            print(f'{unsplitData} US')
-            print(mutedPeople)
         for name in mutedPeople:
             if chat_message.author.name == name:
-                print('cant speak')
+                print('Muted')
             else:
                 print('can speak')
                 #end mute update
+                print('running')
                 if textWords[0] == '!':
                     for word in textWords:
                         if word in hotKeyword:
                             soundQ.append(word)
-        
+
                     while len(textWords) > 0:
-                            textWords.pop(0)
-        
+                        textWords.pop(0)
+
                     print(soundQ)
-                        #if you want you can try and fix the code below, but i will most likely fix it myself
-                        #combine split words
-                        #if len(soundQ) > 1:
-                        #    if soundQ[0] and soundQ[1] == 'womp':
-                        #        soundQ[0] = soundQ[0] + ' ' + soundQ[1]
-                        #       soundQ.pop(1)
+                    #wordJoiner = 0
+                    #if len(soundQ) > 1:
+                    #    for wordA in soundQ:
+                    #        multWord = multi_Long_Keywords[wordJoiner]
+                    #        wordB = soundQ[wordJoiner + 1]
+                    #        if wordA in multWord[wordJoiner]:
+                    #            if wordB in multWord[wordJoiner+1]:
+                    #                combined_word = wordA, ' ', wordB
+                    #                soundQ.pop(wordJoiner+1)
+                    #                soundQ[wordJoiner] = combined_word
+                    #        wordJoiner += 1
                         #tri noise
                     while len(soundQ) > 0:
                         print('running sound')
@@ -64,6 +75,8 @@ while chat.is_alive():
                     recommend.append(r)
                     day = datetime.date.today()
                     with open("recommendations.txt", "a") as file:
-                        file.write(f'{r}, When?: {day}, {chat_message.author.name}')
-                        file.write('      ')
-                    print(recommend)
+                        file.write(f'{r}, When?: {day}')
+                        file.write('        ')
+                        print(recommend)
+                        if hasRecommendationSound:
+                            pyautogui.press('CHANGE')
